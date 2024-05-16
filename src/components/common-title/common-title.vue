be781492ce8a46deadb85ada476f73b90f520ded
Jinliang <miku.cy@foxmail.com>
2024-05-16 21:00:46 +0800

    组件具名插槽定义公共标题模块

--- src/components/common-title/common-title.vue
+++ src/components/common-title/common-title.vue
@@ -1,6 +1,11 @@
 <template>
-  <view>
-    公共标题
+  <view class="common-title">
+    <view class="name">
+      <slot name="name"></slot>
+    </view>
+    <view class="custom">
+      <slot name="custom"></slot>
+    </view>
   </view>
 </template>
 
@@ -10,5 +15,13 @@
 
 
 <style scoped lang="scss">
-
+.common-title{
+  display: flex;
+  justify-content: space-between; /* 两端对齐 */
+  align-items: center;
+  padding: 0 30rpx;
+  .name{
+    font-size: 40rpx;
+  }
+}
 </style>
