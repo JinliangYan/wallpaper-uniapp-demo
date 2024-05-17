883417c2a51d0de48e728b97876a4ef4c30f117c
Jinliang <miku.cy@foxmail.com>
2024-05-17 15:10:26 +0800

    个人中心页面布局

--- src/components/theme-item/theme-item.vue
+++ src/components/theme-item/theme-item.vue
@@ -17,8 +17,6 @@
 </template>
 
 <script setup lang="ts">
-import {defineProps} from "vue"
-
 defineProps({
   isMore:{
     type: Boolean,
--- src/pages/user/user.vue
+++ src/pages/user/user.vue
@@ -1,6 +1,41 @@
 <template>
-  <view>
-    我的
+  <view class="userLayout">
+    <view class="userInfo">
+      <view class="avatar">
+        <image src="../../static/images/xxmLogo.png" mode="aspectFill"></image>
+      </view>
+      <view class="ip">100.100.100.100</view>
+      <view class="address">来自于: 山东</view>
+    </view>
+    <view class="section">
+      <view class="list">
+        <view class="row" v-for="item in 3">
+          <view class="left">
+            <uni-icons type="download-filled" size="20" color="#28b389"></uni-icons>
+            <view class="text">我的下载</view>
+          </view>
+          <view class="right">
+            <view class="text">33</view>
+            <uni-icons type="right" size="15" color="#aaa"></uni-icons>
+          </view>
+        </view>
+      </view>
+    </view>
+
+    <view class="section">
+      <view class="list">
+        <view class="row" v-for="item in 2">
+          <view class="left">
+            <uni-icons type="download-filled" size="20" color="#28b389"></uni-icons>
+            <view class="text">我的下载</view>
+          </view>
+          <view class="right">
+            <view class="text">33</view>
+            <uni-icons type="right" size="15" color="#aaa"></uni-icons>
+          </view>
+        </view>
+      </view>
+    </view>
   </view>
 </template>
 
@@ -10,5 +45,70 @@
 
 
 <style scoped lang="scss">
-
+.userLayout{
+  .userInfo{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    padding: 50rpx 0;
+    .avatar{
+      width: 160rpx;
+      height: 160rpx;
+      border-radius: 50%;
+      overflow: hidden;
+      image{
+        width: 100%;
+        height: 100%;
+      }
+    }
+    .ip{
+      font-size: 44rpx;
+      color: #333;
+      padding: 20rpx 0 5rpx;
+    }
+    .address{
+      font-size: 28rpx;
+      color: #aaa;
+    }
+  }
+  .section{
+    width: 690rpx;
+    margin: 50rpx auto; /* 上下50, 左右居中 */
+    border: 1px solid #eee;
+    border-radius: 10rpx;
+    box-shadow: 0 0 30rpx rgba(0, 0, 0, .05);
+    .list{
+      .row{
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        padding: 0 30rpx;
+        height: 100rpx;
+        border-bottom: 1px solid #eee;
+        /* 最后一个不要有边框, 因为父级有边框, 会导致两个边框出现在底部 */
+        &:last-child{border-bottom: 0}
+        .left{
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          .text{
+            padding-left: 20rpx;
+            color: #666;
+          }
+        }
+        .right{
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          .text{
+            padding-left: 20rpx;
+            font-size: 28rpx;
+            color: #aaa;
+          }
+        }
+      }
+    }
+  }
+}
 </style>
