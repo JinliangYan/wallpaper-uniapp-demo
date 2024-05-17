dd971ba7a15ec4b337ca4e341fbcabe625830337
Jinliang <miku.cy@foxmail.com>
2024-05-17 14:30:36 +0800

    设置项目底部tab页面切换标签

---
+++ src/pages/classify/classify.vue
@@ -0,0 +1,20 @@
+<template>
+  <view>
+    <view class="classify">
+      <theme-item v-for="item in 15"></theme-item>
+    </view>
+  </view>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+
+<style scoped lang="scss">
+.classify{
+  padding: 30rpx;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+}
+</style>
